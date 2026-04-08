7961a771c79d6ee90a444dbd75924fc8fb964c31
VAMSI TARUN ATCHUTA <vtu24588@veltech.edu.in>
2026-04-08 10:55:48 +0530

    Create task 10.java

---
+++ week 10/task 10.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0;
+        int high = letters.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (letters[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return letters[low % letters.length];
+    }
+}

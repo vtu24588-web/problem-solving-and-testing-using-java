4e50ffd060071ebeb4d5da76d6d0af5803353947
VAMSI TARUN ATCHUTA <vtu24588@veltech.edu.in>
2026-04-08 10:52:05 +0530

    Create task 5.java

---
+++ week 10/task 5.java
@@ -0,0 +1,79 @@
+class MyLinkedList {
+    class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        if (size == 0) {
+            addAtHead(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) return;
+        if (index <= 0) {
+            addAtHead(val);
+        } else if (index == size) {
+            addAtTail(val);
+        } else {
+            Node newNode = new Node(val);
+            Node curr = head;
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
+            }
+            newNode.next = curr.next;
+            curr.next = newNode;
+            size++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node curr = head;
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
+            }
+            curr.next = curr.next.next;
+        }
+        size--;
+    }
+}
